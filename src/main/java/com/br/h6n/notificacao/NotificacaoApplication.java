1ec772f62c1f95cbf5d207e4abd5364968071a0a
Hamilton Tumenas Borges <hamiltontubo@gmail.com>
2025-10-03 15:06:18 -0300

    initial commit

---
+++ src/main/java/com/br/h6n/notificacao/NotificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.br.h6n.notificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/h6n/notificacao/NotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.h6n.notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
